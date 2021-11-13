be2776f60df1b7cb92a3c1476770205c3ad0893d
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-13 12:34:18 -0300

    feat(front-end): created Breadcrumb component

---
+++ app/src/components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Icons
+import { IoIosArrowForward } from 'react-icons/io';
+
+// Styles
+import './Breadcrumb.scss';
+
+const Breadcrumb = ({ items }) => {
+  const getComponentClasses = (isLastItemOfList) =>
+    [
+      'breadcrumb__item',
+      isLastItemOfList && 'breadcrumb__item--last-item',
+    ].join(' ');
+
+  const renderItems = () =>
+    items.map((item, index) => {
+      const isLastItemOfList = index === items.length - 1;
+
+      return (
+        <>
+          <li className={getComponentClasses(isLastItemOfList)}>{item}</li>
+          <span className="breadcrumb__separator">
+            {!isLastItemOfList ? <IoIosArrowForward /> : null}
+          </span>
+        </>
+      );
+    });
+
+  return (
+    <nav aria-label="breadcrumb" className="breadcrumb-nav">
+      <ol className="breadcrumb">{renderItems()}</ol>
+    </nav>
+  );
+};
+
+Breadcrumb.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Breadcrumb;
