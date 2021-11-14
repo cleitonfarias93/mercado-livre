7ee7f434e88b8ac0d3beadc81016490fdd3c4b4c
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-14 20:28:17 -0300

    feat(front-end): added ItemDetailComponent

---
+++ app/src/components/ItemDetailComponent/ItemDetailComponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import './ItemDetailComponent.scss';
+
+const ItemDetailComponent = ({ item = {} }) => {
+  console.log('ItemDetailComponent');
+  return (
+    <section className="item-detail-component">
+      <div className="item-detail-component__image">
+        <img src={item.picture} alt="Minha Figura" />
+      </div>
+
+      <div className="item-detail-component__content">
+        <span className="item-detail-component__content__price">
+          $ {item.price?.amount}
+        </span>
+        <span className="item-detail-component__content__name">
+          {item.title}
+        </span>
+        <span className="item-detail-component__content__situation">
+          {item.condition}
+        </span>
+      </div>
+
+      <span className="item-detail-component__address">
+        {item.address?.city_name}
+      </span>
+    </section>
+  );
+};
+
+ItemDetailComponent.propTypes = {
+  item: PropTypes.shape({
+    picture: PropTypes.string,
+    price: PropTypes.shape({
+      amount: PropTypes.number,
+      currency: PropTypes.string,
+    }),
+    title: PropTypes.string,
+    condition: PropTypes.string,
+  }),
+};
+
+ItemDetailComponent.defaultProps = {
+  item: {},
+};
+
+export default ItemDetailComponent;
--- app/src/pages/SearchResult/SearchResult.js
+++ app/src/pages/SearchResult/SearchResult.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import i18n from 'i18next';
 
 // Components
 import Breadcrumb from 'components/Breadcrumb';
 import ItemDetailComponent from 'components/ItemDetailComponent';
+import Spinner from 'components/Spinner';
 
 // Utils
 import { getQueryParametersByURL } from 'utils/queryString';
@@ -18,23 +20,39 @@ const SearchResult = () => {
   const history = useHistory();
 
   const [items, setItems] = useState([]);
+  const [fetchingItems, setFetchingItems] = useState(true);
 
   useEffect(() => {
     const parameters = getQueryParametersByURL(history.location.search);
     fetchItems(parameters);
-  });
+  }, [history.location.search]);
 
   const fetchItems = async (parameters) => {
-    const { search } = parameters;
-    const { data } = await getItems(search);
+    setFetchingItems(true);
 
-    setItems(data);
+    try {
+      const { search } = parameters || {};
+      const { data } = await getItems(search);
+      setItems(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setFetchingItems(false);
+    }
   };
+
+  const renderSearchResult = () => (
+    <>
+      <Breadcrumb items={items.categories} />
+      {items.items.map((item) => (
+        <ItemDetailComponent key={item.id} item={item} />
+      ))}
+    </>
+  );
 
   return (
     <div className="search-result">
-      <Breadcrumb items={items?.categories} />
-      <ItemDetailComponent />
+      {fetchingItems ? <Spinner /> : renderSearchResult()}
     </div>
   );
 };
