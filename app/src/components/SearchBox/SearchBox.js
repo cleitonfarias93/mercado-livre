33f92d85f04ba3d1dc3e488b83e77c2e07797ab4
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-14 20:26:33 -0300

    feat(front-end): added MercadoLivre icon in SearchBox

--- app/src/components/SearchBox/SearchBox.js
+++ app/src/components/SearchBox/SearchBox.js
@@ -5,6 +5,9 @@ import { useHistory } from 'react-router-dom';
 // Utils
 import { convertToQueryString } from 'utils/queryString';
 
+// Icons
+import MercadoLivreIcon from 'components/Icons/MercadoLivreIcon';
+
 // Styles
 import './SearchBox.scss';
 
@@ -13,7 +16,11 @@ const SearchBox = () => {
 
   const [searchInput, setSearchInput] = useState('');
 
-  const handleClick = () => {
+  const handleSubmit = () => {
+    if (!searchInput.trim().length) {
+      return;
+    }
+
     const query = convertToQueryString({
       search: searchInput,
     });
@@ -28,16 +35,36 @@ const SearchBox = () => {
     setSearchInput(event.target.value);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
   return (
     <header className="header">
-      <input
-        className="header__input-search"
-        placeholder={i18n.t('Nunca pare de procurar')}
-        onChange={handleChange}
-      />
-      <button type="button" onClick={handleClick}>
-        {i18n.t('Buscar')}
-      </button>
+      <div className="input-group">
+        <MercadoLivreIcon className="header__icon" />
+        <input
+          type="text"
+          className="header__input-search form-control"
+          placeholder={i18n.t('Nunca pare de procurar')}
+          aria-label={i18n.t('Nunca pare de procurar')}
+          aria-describedby="basic-addon2"
+          onChange={handleChange}
+          onKeyPress={handleKeyDown}
+        />
+        <div className="input-group-append">
+          <button
+            type="button"
+            className="header__input-search__button input-group-text"
+            onClick={handleSubmit}
+            id="basic-addon2"
+          >
+            {i18n.t('Buscar')}
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
