afddf0a68b14be7136d32c3b7453a95a40eeaee8
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-15 17:39:26 -0300

    refactor(front-end): breadcrumb component

--- app/src/components/Breadcrumb/Breadcrumb.js
+++ app/src/components/Breadcrumb/Breadcrumb.js
@@ -8,23 +8,30 @@ import { IoIosArrowForward } from 'react-icons/io';
 import './Breadcrumb.scss';
 
 const Breadcrumb = ({ items = [] }) => {
+  const itemsBreadcrumb = items.map((item, index) => ({
+    label: item,
+    id: index,
+  }));
+
   const getComponentClasses = (isLastItemOfList) =>
     [
-      'breadcrumb__item',
-      isLastItemOfList && 'breadcrumb__item--last-item',
+      'breadcrumb__container__item',
+      isLastItemOfList && 'breadcrumb__container__item--last-item',
     ].join(' ');
 
   const renderItems = () =>
-    items.map((item, index) => {
-      const isLastItemOfList = index === items.length - 1;
+    itemsBreadcrumb.map((item, index) => {
+      const isLastItemOfList = index === itemsBreadcrumb.length - 1;
 
       return (
-        <>
-          <li className={getComponentClasses(isLastItemOfList)}>{item}</li>
-          <span className="breadcrumb__separator">
+        <div className="breadcrumb__container" key={item.id}>
+          <li className={getComponentClasses(isLastItemOfList)}>
+            {item.label}
+          </li>
+          <span className="breadcrumb__container__separator">
             {!isLastItemOfList ? <IoIosArrowForward /> : null}
           </span>
-        </>
+        </div>
       );
     });
 
@@ -36,7 +43,11 @@ const Breadcrumb = ({ items = [] }) => {
 };
 
 Breadcrumb.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  items: PropTypes.arrayOf(PropTypes.string),
+};
+
+Breadcrumb.defaultProps = {
+  items: [''],
 };
 
 export default Breadcrumb;
