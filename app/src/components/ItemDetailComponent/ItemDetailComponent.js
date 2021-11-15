f1482a5ce6d36f386018e03b73bf689ae5e64c83
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-15 15:41:05 -0300

    feat(front-end): added navigation to item details

--- app/src/components/ItemDetailComponent/ItemDetailComponent.js
+++ app/src/components/ItemDetailComponent/ItemDetailComponent.js
@@ -1,30 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
+// Components
+import PriceDisplay from 'components/PriceDisplay';
 
 // Styles
 import './ItemDetailComponent.scss';
 
-const ItemDetailComponent = ({ item = {} }) => (
-  <section className="item-detail-component">
-    <div className="item-detail-component__image">
-      <img src={item.picture} alt="Minha Figura" />
-    </div>
+const ItemDetailComponent = ({ item = {} }) => {
+  const history = useHistory();
 
-    <div className="item-detail-component__content">
-      <span className="item-detail-component__content__price">
-        $ {item.price?.amount}
-      </span>
-      <span className="item-detail-component__content__name">{item.title}</span>
-      <span className="item-detail-component__content__situation">
-        {item.condition}
-      </span>
-    </div>
+  const goToItemDetails = () => {
+    history.push(`/items/${item.id}`);
+  };
 
-    <span className="item-detail-component__address">
-      {item.address?.city_name}
-    </span>
-  </section>
-);
+  const renderPrice = () => {
+    const amount = new Intl.NumberFormat('pt-BR').format(item.price?.amount);
+    const decimals = item.price?.decimals;
+
+    return (
+      <PriceDisplay
+        className="item-detail-component__content__price"
+        amount={amount}
+        decimals={decimals}
+        symbol="$"
+      />
+    );
+  };
+
+  return (
+    <section
+      className="item-detail-component"
+      onClick={goToItemDetails}
+      aria-hidden="true"
+    >
+      <div className="item-detail-component__image">
+        <img src={item.picture} alt={item.title} />
+      </div>
+
+      <div className="item-detail-component__content">
+        {renderPrice()}
+        <span className="item-detail-component__content__name">
+          {item.title}
+        </span>
+      </div>
+
+      <span className="item-detail-component__address">
+        {item.address?.city_name}
+      </span>
+    </section>
+  );
+};
 
 ItemDetailComponent.propTypes = {
   item: PropTypes.shape({
